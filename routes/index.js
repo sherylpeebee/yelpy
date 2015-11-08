f3e2c43beaa15d87ab7c700bf59e5b398402d0b5
Sheryl Boughton <sherpbo@gmail.com>
2015-11-07 19:52:10 -0800

    include yelp api endpoint

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 var secrets = require('../secrets.js');
 
-var request_data = {
-    url: 'https://api.yelp.com/v2/search/?location=San Francisco, CA',
-    method: 'GET'
-};
-
 var yelp = require("yelp").createClient({
   consumer_key: secrets.CONSUMER_KEY,
   consumer_secret: secrets.CONSUMER_SECRET,
@@ -14,18 +9,36 @@ var yelp = require("yelp").createClient({
   token_secret: secrets.TOKEN_SECRET
 });
 
-
-yelp.search({term: "sports", location: "San Jose", limit: 3, radius_filter: 16000}, function(error, data) {
-  //max radius is 40000 meters or 25 miles. to start i want to keep everything within 10 miles of current location
-  //maybe later give option to toggle between manual location and current location
-  if(error){
-    console.log(error);
-  }
-  console.log(data);
-});
-
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/yelpSearch/:ll?', function(req, res, next) {
+  if(!req.query.location && !req.params.ll){
+    res.status(400).send({error: 'We weren\'t able to determine your location. Please try again.'});
+  }
+  else if(req.query.location && req.params.ll){
+    res.status(400).send({error: 'Please specify a location.'});
+  }
+  var zoneTerm = req.params.ll ? 'll' : 'location',
+      zone = req.params.ll ? req.params.ll : req.query.location;
+      //'ll' accepts a comma separated long and lat, e.g.: ll:'33.788022, -102.399797'
+  var request_data = {
+    term: req.query.term ? req.query.term: null,//may need to decode uri component here
+    accuracy: 9,
+    // limit: 20,
+    radius_filter: 16000//in meters --> 20 miles; max is 40000 meters
+  };
+  request_data[zoneTerm] = zone;
+
+  yelp.search(request_data, function(error, data) {
+    if(error){
+      console.log(error);
+    }
+    console.log(data);
+    res.status(200).json(data);
+  });
+});
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.put('/:id/toggleAvailability', function(req, res, next) {
+  var id = req.params.id;
+  var available = req.body.availability;
+});
+
 module.exports = router;
